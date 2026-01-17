e226b31cd01048b16e7afb5c6d19f0a6bda95f79
Roy McKenzie <roy@fervid.digital>
2026-01-17 14:46:59 -0700

    Initial Commit

---
+++ SwiftTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftTest
+//
+//  Created by Roy McKenzie on 1/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftTest/SwiftTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTestApp.swift
+//  SwiftTest
+//
+//  Created by Roy McKenzie on 1/17/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
